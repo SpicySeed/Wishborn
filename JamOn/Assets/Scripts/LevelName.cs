31ad04845e9ce69ca854556157efc1d78bc6b2e5
xRyusey <ismafern@ucm.es>
2021-04-18 19:06:35 +0200

    Nombres de niveles y boss

---
+++ JamOn/Assets/Scripts/LevelName.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelName : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+    [SerializeField] private float delayTime = 1;
+    [SerializeField] private float stayTime = 1;
+    private bool start = false;
+    private bool end = false;
+    private float time = 0;
+
+    void Update()
+    {
+        if (!start && time < delayTime)
+        {
+            time += Time.deltaTime;
+            if (time > delayTime)
+            {
+                start = true;
+                time = 0;
+                anim.Play("NameAppear");
+            }
+        }
+
+        if (start && !end && time < stayTime)
+        {
+            time += Time.deltaTime;
+            if (time > stayTime)
+            {
+                end = true;
+                anim.Play("NameDisappear");
+            }
+        }
+    }
+}
